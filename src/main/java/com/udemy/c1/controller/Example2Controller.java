4943ad9ff1eb1b0ff5ff80f6e110e2e9cf8f9b31
Yanne <yaneth.arguello@meltsan.com>
2022-02-17 16:47:34 -0600

    url amigables

--- src/main/java/com/udemy/c1/controller/Example2Controller.java
+++ src/main/java/com/udemy/c1/controller/Example2Controller.java
@@ -2,6 +2,7 @@ package com.udemy.c1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,10 +16,19 @@ public class Example2Controller {
     //localhost:8080/example2/request1?nm=JON&eg=22
     //localhost:8080/example2/request1?nm=MYKE&eg=23
     @GetMapping("/request1")
-    public ModelAndView request1(@RequestParam(name="nm") String name, @RequestParam(name="eg") String age){
+    public ModelAndView request1(@RequestParam(name="nm", required = false) String name, @RequestParam(name="eg", required = false) String age){
         ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
         mav.addObject("nm_modal", name);
         mav.addObject("eg", age);
+        return mav;
+    }
+
+    //localhost:8080/example2/request1/JON/22
+    @GetMapping("/request2/{nm}/{eg}")
+    public ModelAndView request2(@PathVariable(value = "nm", required = false) String name, @PathVariable(value = "eg",required = false) String ege){
+        ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+        mav.addObject("nm_modal", name);
+        mav.addObject("eg", ege);
         return mav;
     }
 }
